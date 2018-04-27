82f1219bcf5a036f7ec71f1e0d29ab168debe4eb
T Hage <tommhage@gmail.com>
2018-04-27 15:07:26 +0200

    added CORS filter for frontend

--- src/main/kotlin/com/tom/Application.kt
+++ src/main/kotlin/com/tom/Application.kt
@@ -3,7 +3,11 @@ package com.tom
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.ServletContextHandler
 import org.eclipse.jetty.servlet.ServletHolder
+import org.eclipse.jetty.servlets.CrossOriginFilter
 import org.glassfish.jersey.servlet.ServletContainer
+import java.util.*
+import javax.servlet.DispatcherType
+
 
 fun main(args: Array<String>) {
     val server: Server = Server(8080)
@@ -11,6 +15,13 @@ fun main(args: Array<String>) {
 
     ctx.contextPath = "/"
     server.handler = ctx
+
+    val cors = ctx.addFilter(CrossOriginFilter::class.java, "/*", EnumSet.of<DispatcherType>(DispatcherType.REQUEST))
+    cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "http://localhost:4200")
+    cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "http://localhost:4200")
+    cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD")
+    cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin")
+
 
     val servletHolder: ServletHolder = ctx.addServlet(ServletContainer::class.java, "/api/*")
     servletHolder.initOrder = 1
