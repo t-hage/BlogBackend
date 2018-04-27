4583f5e6a3ed089c355daf81b3434cbff2a8f218
T Hage <tommhage@gmail.com>
2018-04-27 10:37:27 +0200

    added html in endpoint

--- src/main/kotlin/com/tom/BlogPostResource.kt
+++ src/main/kotlin/com/tom/BlogPostResource.kt
@@ -2,6 +2,7 @@ package com.tom
 
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
@@ -13,6 +14,20 @@ class BlogPostResource {
     fun message(): String {
         val s = "Hello World"
         return s
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("{id}")
+    fun get(@PathParam("id") id: Int): String {
+
+        val defaultBlogpost = """
+            <h1>First Blog Post</h1>
+            <p>Hi There! This is the first post. The id is $id<p>
+            <p>Goodbye!<p>
+        """.trimIndent()
+
+        return defaultBlogpost
     }
 
 
